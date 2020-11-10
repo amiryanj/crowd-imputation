cd711269a619acaa5a27afaf154172f99eaf1000
Javad <javad.amirian@inria.fr>
2020-11-10 22:30:44 +0100

    added `still` flow class

--- src/followbot/robot_functions/flow_classifier.py
+++ src/followbot/robot_functions/flow_classifier.py
@@ -14,17 +14,26 @@ class FlowClass:
 class FlowClassifier:
     def __init__(self):
         self.classes = []
-        self.preset_flow_classes = {0: FlowClass(0, 'to_right', 'r'),
-                                    1: FlowClass(1, 'to_left', 'b')}
+        self.preset_flow_classes = {0: FlowClass(0, 'still', 'g'),
+                                    1: FlowClass(1, 'to_right', 'r'),
+                                    2: FlowClass(2, 'to_left', 'b'),
+                                    }
 
     def classify(self, agents_locs, agents_vel):
         output = []
-        agents_orien = np.arctan2(agents_vel[:, 1], agents_vel[:, 0])
+        if len(agents_vel):
+            agents_orien = np.arctan2(agents_vel[:, 1], agents_vel[:, 0])
 
-        for i in range(len(agents_orien)):
-            towards_R = (-np.pi / 2 <= agents_orien[i] < np.pi / 2)
-            output.append(self.preset_flow_classes[0] if towards_R
-                          else self.preset_flow_classes[1])
+            for i in range(len(agents_orien)):
+                # still agent
+                if np.linalg.norm(agents_vel[i]) < 0.2:
+                    output.append(self.preset_flow_classes[0])
+                # towards_R agent
+                elif -np.pi / 2 <= agents_orien[i] < np.pi / 2:
+                    output.append(self.preset_flow_classes[1])
+                # towards_L agent
+                else:
+                    output.append(self.preset_flow_classes[2])
         return output
 
     def id2color(self, ids):
