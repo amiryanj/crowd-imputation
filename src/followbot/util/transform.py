2ea1136b8f4c35628f3b5eb1e7ad2ae82f8ad452
Javad <javad.amirian@inria.fr>
2021-02-26 02:09:38 +0100

    inverse of transform

--- src/followbot/util/transform.py
+++ src/followbot/util/transform.py
@@ -8,6 +8,24 @@ class Transform:
         self.rotation = rot_
 
     def apply(self, pos, rot):
-        out_orien = rot.__mul__(self.rotation).as_quat()
+        out_orien = (self.rotation * rot).as_rotvec()
         out_trans = self.translation + self.rotation.apply(pos)
         return out_trans, out_orien
+
+    def inverse(self, pos, rot):
+        out_orien = (self.rotation.inv() * rot).as_rotvec()
+        out_trans = self.rotation.inv().apply(pos - self.translation)
+        return out_trans, out_orien
+
+
+# test
+if __name__ == "__main__":
+    orien = Rotation.from_euler('z', np.pi/8, degrees=False)
+    pos = np.array([2, 4, 0])
+
+    t = Transform(np.array([3, 6, 5]), Rotation.from_euler('z', np.pi/8))
+    print(pos, orien.as_rotvec())
+    pos_tf, orien_tf = t.apply(pos, orien)
+    print(pos_tf, orien_tf)
+    pos_tf_tf, orien_tf_tf = t.inverse(pos_tf, Rotation.from_rotvec(orien_tf))
+    print(pos_tf_tf, orien_tf_tf)
